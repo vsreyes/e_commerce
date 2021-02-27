c361b652d50efb8fd58249486cac9b6ec5e75258
vsreyes <virgilio.s.reyes@gmail.com>
2021-02-27 23:54:14 +0100

    Product setup

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typegraphy, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Product
