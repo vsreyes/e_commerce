6fd7be98b1057da7e0ccf3f49ee63147f3cda3a9
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-01 01:23:16 +0100

    Navbar setup

---
+++ src/components/Products/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+
+import logo from '../../assets/commerce.png'
+
+const Navbar = () => {
+  return (
+    <>
+      <AppBar  position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
+            Commerce.js
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default Navbar
