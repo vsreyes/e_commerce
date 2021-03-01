15560652071f081de10d79f12629603decc3c774
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-01 01:27:47 +0100

    Navbar setup

--- src/components/Products/Navbar/Navbar.jsx
+++ src/components/Products/Navbar/Navbar.jsx
@@ -13,6 +13,14 @@ const Navbar = () => {
             <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
             Commerce.js
           </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton aria-label="Show cart items" color="inherit">
+                <Badge badgeContent={2} color="secondary">
+                  <ShoppingCart />
+                </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </>
