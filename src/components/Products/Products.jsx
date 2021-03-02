e78f675be9526a664a541a7838478a40fbe678ad
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-02 15:20:20 +0100

    Product setup

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  { id: 1, name: 'Shoes', description: 'running shoes.', price: '$5', image: ''},
-  { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: ''},
-]
-
-const Products = () => {
+const Products = ( products ) => {
   const classes = useStyles();
 
   return (
