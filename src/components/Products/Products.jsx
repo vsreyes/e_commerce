25dd5d26a80c484b577cef6c5a6ed55155607921
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-02 19:48:27 +0100

    Cart setup

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const Products = ( products ) => {
+const Products = ({ products, onAddToCart }) => {
   const classes = useStyles();
 
   return (
@@ -12,8 +12,8 @@ const Products = ( products ) => {
       <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
           {products.map((product) => (
-              <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                <Product product={product} />
+              <Grid key={product.id} xs={12} sm={6} md={4} lg={3}>
+                <Product product={product} onAddToCart={onAddToCart} />
               </Grid>
           ))}
       </Grid>
