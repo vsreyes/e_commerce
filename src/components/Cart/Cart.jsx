ef6e97e4e9ecd723cdd39764716bbf6e26e1cb01
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-03 14:58:03 +0100

    Cart setup

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
+import classes from '*.module.css';
+
+import useStyles from './styles';
 
 const Cart = ({ cart }) => {
   const isEmpty = !cart.line_items.length;
+  const classes = useStyles();
 
   const EmptyCart = () => (
     <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
@@ -13,9 +17,17 @@ const Cart = ({ cart }) => {
       <Grid container spacing ={3}>
           {cart.line_items.map((item) => (
               <Grid item xs={12} sm={4} key={item.id}>
+                  <div>{item.name}</div>
               </Grid>
           ))}
       </Grid>
+      <div className={classes.cardDetails}>
+            <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
+            <div>
+              <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+              <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
+            </div>
+      </div>
     </>
   )
 
