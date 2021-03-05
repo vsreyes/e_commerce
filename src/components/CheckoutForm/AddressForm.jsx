451c2c3bb92de928e468238bd1d7d8ac9fd3e90b
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-05 15:15:03 +0100

    Address Form setup

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import  { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import FormInput from './CustomTextField';
 
 const AddressForm = () => {
+  const methods = useForm();
+
   return (
-    <div>
-      AddressForm
-    </div>
+    <>
+      <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit=''>
+            <Grid container spacing={3}>
+              <FormInput required name='FirstName' label='First Name' />
+            </Grid>
+        </form>
+      </FormProvider>
+    </>
   )
 }
 
