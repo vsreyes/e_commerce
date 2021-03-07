dea99bb83eb646ece09a3b9787ea9f7c656204c2
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-07 16:34:38 +0100

    Review

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -11,6 +11,7 @@ const steps = ['Shipping address', 'Payment details'];
 const Checkout = ({ cart }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [checkoutToken, setCheckoutToken] = useState(null);
+  const [shippingData, setShippingData] = useState({})
   const classes = useStyles();
 
   useEffect(() => {
@@ -18,7 +19,7 @@ const Checkout = ({ cart }) => {
       try {
         const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
         
-       
+       console.log(token);
 
         setCheckoutToken(token);
       } catch (error) {
@@ -29,6 +30,15 @@ const Checkout = ({ cart }) => {
     generateToken();
   }, [cart]);
 
+  const nextStep = () => setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  const backStep = () => setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+  const next = (data) => {
+    setShippingData(data);
+
+    nextStep();
+  }
+
 const Confirmation = () => (
   <div>
     Confirmation
@@ -36,8 +46,8 @@ const Confirmation = () => (
 );
 
 const Form = () => activeStep === 0
-  ? <AddressForm checkoutToken={checkoutToken} />
-  : <PaymentForm />
+  ? <AddressForm checkoutToken={checkoutToken} next={next} />
+  : <PaymentForm shippingData={shippingData} />
 
   return (
     <>
